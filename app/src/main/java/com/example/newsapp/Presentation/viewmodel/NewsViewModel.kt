1063cd74d80b7df9d5c1f6365b77222bb627288d
Abdelrahman Elgendy <abdelrahmanelgendy961@gmail.com>
2025-04-19 14:25:23 +0200

    Clean Architecture

--- app/src/main/java/com/example/newsapp/Presentation/viewmodel/NewsViewModel.kt
+++ app/src/main/java/com/example/newsapp/Presentation/viewmodel/NewsViewModel.kt
@@ -1,4 +1,4 @@
-package com.example.newsapp.Presentation
+package com.example.newsapp.Presentation.viewmodel
 
 import android.app.Application
 import androidx.compose.runtime.mutableStateListOf
@@ -20,62 +20,6 @@ import kotlinx.coroutines.launch
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import java.io.IOException
-
-/**
-class NewsViewModel : ViewModel() {
-private val apiKey = "344e477ac241420db1e7501341b3ff48"
-
-private val retrofit = Retrofit.Builder()
-.baseUrl("https://newsapi.org/")
-.addConverterFactory(GsonConverterFactory.create())
-.build()
-
-private val api = retrofit.create(NewsApiService::class.java)
-
-var topicNews = mutableStateOf<List<Article>>(emptyList())
-private set
-
-var breakingNews = mutableStateOf<List<Article>>(emptyList())
-private set
-
-var isLoading = mutableStateOf(false)
-var errorMessage = mutableStateOf<String?>(null)
-
-private val _favorites = mutableStateListOf<Article>()
-val favorites: List<Article> get() = _favorites
-
-
-fun toggleFavorite(article: Article) {
-if (_favorites.contains(article)) _favorites.remove(article)
-else _favorites.add(article)
-}
-
-fun isFavorite(article: Article): Boolean = _favorites.contains(article)
-
-fun fetchNews(topic: String = "technology") {
-viewModelScope.launch {
-isLoading.value = true
-errorMessage.value = null
-
-try {
-val topicResponse = api.getTopicNews(topic, apiKey)
-val breakingResponse = api.getBreakingNews(apiKey = apiKey)
-
-if (topicResponse.isSuccessful && breakingResponse.isSuccessful) {
-topicNews.value = topicResponse.body()?.articles ?: emptyList()
-breakingNews.value = breakingResponse.body()?.articles ?: emptyList()
-} else {
-errorMessage.value = "Failed to fetch news: ${topicResponse.message()}"
-}
-} catch (e: Exception) {
-errorMessage.value = "Error: ${e.localizedMessage}"
-} finally {
-isLoading.value = false
-}
-}
-}
-}
- **/
 
 class NewsViewModel(application: Application) : AndroidViewModel(application) {
 
